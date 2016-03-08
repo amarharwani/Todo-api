d12815dba751bfc361bd7de9da627acfdde1d046
Amar harwani <amar.harwaniQsigma-systems.com>
2016-03-08 17:07:52 +0530

    Add/todos and /todos/:id

--- server.js
+++ server.js
@@ -3,8 +3,43 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 
+var todos = [{
+	id: 1,
+	description: 'meet mom for lunch',
+	completed: false
+}, {
+	id: 2,
+	description: 'Go to Market',
+	completed: false
+},
+ {
+	id: 3,
+	description: 'Had shopping',
+	completed: true
+}];
+
 app.get('/', function (req, res) {
-	res.send('Todo API Root');
+	res.send('Lets Go');
+});
+app.get('/todos', function (req, res) {
+	res.json(todos);
+});
+
+app.get('/todos/:id', function (req, res) {
+
+	var todoId = parseInt(req.params.id,10);
+	var matchedTodos;
+	todos.forEach(function (todo) {
+		if(todoId === todo.id) {
+			matchedTodos = todo;
+		}
+	});
+	if (matchedTodos) {
+		res.json(matchedTodos);
+	} else {
+		res.status(404).send();
+	}
+	
 });
 // two agr for below function port and callback function to execute when everything is done
 app.listen (PORT, function () {
