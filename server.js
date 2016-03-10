c9615a25035895beefa0024a99fe70855bfc2d23
Amar harwani <amar.harwaniQsigma-systems.com>
2016-03-10 15:43:31 +0530

    Add POST for todos

--- server.js
+++ server.js
@@ -1,26 +1,19 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 3000;
 
+var todoNextId = 1;
+// for post method to send req with along to server
+app.use(bodyParser.json());
 
-var todos = [{
-	id: 1,
-	description: 'meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to Market',
-	completed: false
-},
- {
-	id: 3,
-	description: 'Had shopping',
-	completed: true
-}];
+
+var todos = [];
 
 app.get('/', function (req, res) {
 	res.send('Lets Go');
 });
+//to call get method (/todos)
 app.get('/todos', function (req, res) {
 	res.json(todos);
 });
@@ -40,6 +33,15 @@ app.get('/todos/:id', function (req, res) {
 		res.status(404).send();
 	}
 	
+});
+//server getting request(via post method) with json data its doing something with it and sending back to us 
+app.post('/todos', function (req, res) {
+	var body = req.body;
+	body.id = todoNextId++;
+	todos.push(body);
+	console.log('description ' + body.description);
+
+	res.json(body);
 });
 // two agr for below function port and callback function to execute when everything is done
 app.listen (PORT, function () {
