bb9b9cf56f60aed944976aa628de8c7c4f655f4d
Amar harwani <amar.harwaniQsigma-systems.com>
2016-03-14 16:59:13 +0530

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var PORT = process.env.PORT || 3000;
 
 var todoNextId = 1;
@@ -15,18 +16,23 @@ app.get('/', function (req, res) {
 });
 //to call get method (/todos)
 app.get('/todos', function (req, res) {
+	// mind - the below todos is an array here 
 	res.json(todos);
 });
 
 app.get('/todos/:id', function (req, res) {
 
 	var todoId = parseInt(req.params.id,10);
-	var matchedTodos;
-	todos.forEach(function (todo) {
-		if(todoId === todo.id) {
-			matchedTodos = todo;
-		}
-	});
+	
+	////////////////// to refactor using  underscore's function like  findWhere to return item in an array that matches the condition 
+var matchedTodos = _.findWhere(todos, {id: todoId});
+
+	///////////////
+	// todos.forEach(function (todo) {
+	// 	if(todoId === todo.id) {
+	// 		matchedTodos = todo;
+	// 	}
+	// });
 	if (matchedTodos) {
 		res.json(matchedTodos);
 	} else {
@@ -36,7 +42,13 @@ app.get('/todos/:id', function (req, res) {
 });
 //server getting request(via post method) with json data its doing something with it and sending back to us 
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body =  _.pick(req.body, 'description', 'completed') ;
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		//.send will return empty body
+		return res.status(400).send();
+	}
+body.description = body.description.trim();
+
 	body.id = todoNextId++;
 	todos.push(body);
 	console.log('description ' + body.description);
